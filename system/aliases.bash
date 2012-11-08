09611cb871d80eda3b7e4d1ec25d4d30e61fe16a
Filipe Guerra <alias.mac@gmail.com>
2012-11-08 16:39:25 +0100

    Doing dotfiles :)

---
+++ system/aliases.bash
@@ -0,0 +1,16 @@
+# for colors
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
+# alias for listing
+alias ll='ls -lah'
+alias la='ls -a'
+alias l='ls -lh'
+
+# alias for disk tools
+alias du='du -h'
+
+# alias for copy move and remove
+alias cp='cp -v'
+alias mv='mv -v'
+alias rm='rm -v'
