f5f820f7b847b683df53ab1e8d1f46bfa35363b6
Filipe Guerra <alias.mac@gmail.com>
2012-11-13 03:50:15 +0100

    Add dev and system wide apps to open on browser

---
+++ software/dev.sh
@@ -0,0 +1,24 @@
+#
+# Open some browser pages for applications that you might want to install.
+#
+
+# LAMP
+open http://www.mamp.info/en/index.html
+open http://www.sequelpro.com/download
+
+# IDEs
+open http://netbeans.org/downloads/
+open http://www.jetbrains.com/phpstorm/download/
+
+# git GUI
+open http://www.sourcetreeapp.com
+
+# quick editors
+open http://www.sublimetext.com
+
+# BTT
+open http://blog.boastr.net
+
+# Dev Browsers
+open https://www.google.com/intl/en/chrome/browser/
+open http://www.mozilla.org/en-US/firefox
---
+++ software/system.sh
@@ -0,0 +1,9 @@
+#
+# Open some browser pages for applications that you might want to install.
+#
+
+# Trim enabler for SSD drives only
+open http://www.groths.org/?page_id=322
+
+# Skype
+open http://www.skype.com/intl/en/get-skype/
