9f9386d46e94fec35876e70e812a60481685ec98
Filipe Guerra <alias.mac@gmail.com>
2012-11-13 04:08:01 +0100

    Dock settings to magnification and sizes
    - made documentation review
    - reload dock after settings being applied

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -15,7 +15,7 @@
 defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Displays.menu" "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Volume.menu" "/System/Library/CoreServices/Menu Extras/TextInput.menu" "/System/Library/CoreServices/Menu Extras/Battery.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu" "/System/Library/CoreServices/Menu Extras/User.menu"
 
 
-# Set the clock settings
+# Set the clock settings (System Preferences → Date & Time → Clock)
 defaults write com.apple.menuextra.clock DateFormat -string "EE H:mm:ss"
 defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
 defaults write com.apple.menuextra.clock IsAnalog -bool false
@@ -28,17 +28,17 @@ defaults write NSGlobalDomain NSSpellCheckerAutomaticallyIdentifiesLanguages -bo
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input
 ##
 
-# Magic Mouse: enable 2 
+# Magic Mouse: enable 2nd mouse click (System Preferences → Mouse)
 defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
 
-# Enable full keyboard access for all controls
+# Enable full keyboard access for all controls (System Preferences → Keyboard → Keyboard Shortcuts)
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate (System Preferences → Keyboard)
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Set language and text formats
+# Set language and text formats (System Preferences → Language & Text)
 # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
 # `Inches`, and `true` with `false`.
 defaults write NSGlobalDomain AppleLanguages -array "en" "pt_PT" "pt"
@@ -47,11 +47,23 @@ defaults write NSGlobalDomain AppleLanguages -array "en" "pt_PT" "pt"
 #defaults write NSGlobalDomain AppleMetricUnits -bool true
 
 ##
-# Finder
+# Dock
 ##
 
-# Finder: Minimize on double click
+# Minimize on double click (System Preferences → Dock)
 defaults write NSGlobalDomain AppleMiniaturizeOnDoubleClick -bool true
+
+# Minimize to application (System Preferences → Dock)
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Magnification and sizes
+defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock tilesize -int 45
+defaults write com.apple.dock largesize -int 75
+
+##
+# Finder
+##
 
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -95,7 +107,7 @@ defaults write com.apple.terminal "Startup Window Settings" -string "Bond"
 # Kill affected applications
 ##
 
-for app in "Finder" "BetterTouchTool"; do
+for app in "Dock" "Finder" "BetterTouchTool"; do
     killall "$app" > /dev/null 2>&1
 done
 
