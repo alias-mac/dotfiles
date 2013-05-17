1884f0badce534acff7738ec43418e36dc4cc9bf
Filipe Guerra <alias.mac@gmail.com>
2013-05-17 22:39:30 +0200

    PHP MAMP auto detect based on major version
    This way we can upgrade MAMP and it still works :)

--- mamp/path.bash
+++ mamp/path.bash
@@ -1,5 +1,12 @@
 
+# change here for the 5.x version you want
+phpMajorVersion="5.3"
+
+# detect the php minor version based on major version
+phpMinorVersion=$(ls /Applications/MAMP/bin/php/ | grep "$phpMajorVersion" | awk -F. '{print $NF}')
+phpVersion="$phpMajorVersion.$phpMinorVersion"
+
 # MAMP mysql
 PATH=/Applications/MAMP/Library/bin:$PATH
-# MAMP pear, php, phpunit, etc. of version php 5.3.x
-PATH=/Applications/MAMP/bin/php/php5.3.14/bin:$PATH
+# MAMP pear, php, phpunit, etc. of version php 5.x (defined above)
+PATH=/Applications/MAMP/bin/php/php$phpVersion/bin:$PATH
