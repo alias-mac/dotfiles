e5d0149acf934525d9781e87dbd64715dfc11114
Filipe Guerra <alias.mac@gmail.com>
2013-06-12 03:08:51 +0100

    Use brew to install git (and keep it updated - a lot easier)

--- git/git.bash
+++ git/git.bash
@@ -2,19 +2,6 @@
 # Nice git prompt for massive git usage
 #
 
-prompt=/usr/local/git/contrib/completion/git-prompt.sh
-if [[ -a $prompt ]]
-then
-  source $prompt
-  GIT_PS1_SHOWDIRTYSTATE=true
-  GIT_PS1_SHOWSTASHSTATE=true
-  GIT_PS1_SHOWUNTRACKEDFILES=true
-  GIT_PS1_SHOWUPSTREAM=true
-  GIT_PS1_SHOWCOLORHINTS=true
-  PROMPT_COMMAND='__git_ps1 "\h:\W \u" "\\\$ "'
-  return
-fi
-
 prompt=/usr/local/etc/bash_completion.d/git-prompt.sh
 if [[ -a $prompt ]]
 then
--- system/path.bash
+++ system/path.bash
@@ -2,8 +2,5 @@
 # local bin
 PATH=/usr/local/bin:$PATH
 
-# Custom Git version
-PATH=/usr/local/git/bin:$PATH
-
 # .dotfiles bin
 PATH=$DOT/bin:$PATH
