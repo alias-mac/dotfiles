c51b0b483e23a4ef736bd3857151cc4f7c773ac6
Filipe Guerra <alias.mac@gmail.com>
2014-11-16 22:50:47 -0800

    Add cool notification function to be used in terminal

--- bash/bashrc.bash
+++ bash/bashrc.bash
@@ -24,3 +24,15 @@ shopt -s histappend
 
 # Save and reload the history after each command finishes
 #export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
+function notify() {
+  local title="$1"
+  local subtitle="$2"
+  shift 2
+  terminal-notifier -activate com.googlecode.iterm2 -title "$title" -subtitle "$subtitle" -message "$*"
+}
+
+function notifyAndSay() {
+  notify "$@"
+  say "$@"
+}
