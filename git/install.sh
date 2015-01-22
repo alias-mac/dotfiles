9730a3c047213d2e217f8ac037edfb279ce5891a
Filipe Guerra <alias.mac@gmail.com>
2015-01-22 02:45:04 -0800

    Git install for easier deploy by other users

---
+++ git/install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Homebrew
+#
+# This installs the git default configuration. You are free to change it after
+# and keep it in sync if you want.
+#
+
+if ! [ -f git/gitconfig.symlink ]
+then
+  info 'setup gitconfig'
+
+  git_credential='cache'
+  if [ "$(uname -s)" == "Darwin" ]
+  then
+    git_credential='osxkeychain'
+  fi
+
+  user ' - What is your github author name?'
+  read -e git_authorname
+  user ' - What is your github author email?'
+  read -e git_authoremail
+
+  sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
+
+  success 'gitconfig'
+fi
