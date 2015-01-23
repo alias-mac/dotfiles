4e923fd718af271d9d21582236e891b13e477335
Filipe Guerra <alias.mac@gmail.com>
2015-01-23 00:19:18 -0800

    On OS X with case sensitive disk the Terminal is uppercase

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -215,8 +215,8 @@ defaults write com.apple.terminal StringEncodings -array 4
 # Use a modified version of the Pro theme by default in Terminal.app
 open "$DOT/osx/Bond.terminal"
 sleep 1 # Wait a bit to make sure the theme is loaded
-defaults write com.apple.terminal "Default Window Settings" -string "Bond"
-defaults write com.apple.terminal "Startup Window Settings" -string "Bond"
+defaults write com.apple.Terminal "Default Window Settings" -string "Bond"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Bond"
 
 # Enable "focus follows mouse" for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first
