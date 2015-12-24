5e74d626dfcf56dd319d98c7be96fbb6c52f33ad
Filipe Guerra <alias.mac@gmail.com>
2015-12-23 17:39:40 -0800

    Ask password for install during bootstrap

--- osx/install.sh
+++ osx/install.sh
@@ -7,12 +7,6 @@ then
   return
 fi
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 $DOT/osx/set-defaults.sh
 
 user ' - Do you want to enable Remote Login with SSH?'
