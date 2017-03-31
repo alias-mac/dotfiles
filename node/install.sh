8927aada1093a7bef6320b4418e6f68ec663836f
Filipe Guerra <alias.mac@gmail.com>
2017-03-30 22:32:43 -0700

    Node 6 and 7 installs NPM automatically

--- node/install.sh
+++ node/install.sh
@@ -4,7 +4,4 @@ if test ! $(which node) -a $(which brew)
 then
   inform 'Installing node for you.'
   brew install node > /tmp/node-install.log
-
-  inform 'Installing npm for you.'
-  curl -L https://npmjs.org/install.sh | sh
 fi
