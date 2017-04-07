bbecda5e57267c8b37f67737bd6ae66b40afc29d
Filipe Guerra <alias.mac@gmail.com>
2017-04-06 20:24:23 -0700

    I no longer use twitter app

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -246,31 +246,6 @@ defaults write com.apple.Terminal "Startup Window Settings" -string "Bond"
 defaults write com.apple.ActivityMonitor.plist IconType -int 6
 
 ##
-# Twitter.app
-##
-
-# Disable smart quotes as it’s annoying for code tweets
-defaults write com.twitter.twitter-mac AutomaticQuoteSubstitutionEnabled -bool false
-
-# Show the app window when clicking the menu icon
-defaults write com.twitter.twitter-mac MenuItemBehavior -int 1
-
-# Enable the hidden ‘Develop’ menu
-defaults write com.twitter.twitter-mac ShowDevelopMenu -bool true
-
-# Open links in the background
-defaults write com.twitter.twitter-mac openLinksInBackground -bool true
-
-# Allow closing the ‘new tweet’ window by pressing `Esc`
-defaults write com.twitter.twitter-mac ESCClosesComposeWindow -bool true
-
-# Show full names rather than Twitter handles
-defaults write com.twitter.twitter-mac ShowFullNames -bool true
-
-# Hide the app in the background if it’s not the front-most window
-defaults write com.twitter.twitter-mac HideInBackground -bool true
-
-##
 # GPG tools (Mail)
 ##
 defaults write org.gpgtools.gpgmail DefaultSecurityMethod -int 2
