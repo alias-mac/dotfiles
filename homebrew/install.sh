69ad7b562108e8454027d046a6ca30129bf67bc6
Filipe Guerra <alias.mac@gmail.com>
2017-07-27 15:25:03 -0700

    Linuxbrew support during install

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,20 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
 # Homebrew
 #
 # If we're on a Mac, let's install http://brew.sh and setup some of the common
 # dependencies needed (or at least desired) for us.
+# If we are on Linux, install Linuxbrew only (no bundle install).
 #
 
-if [ "$(uname -s)" != "Darwin" ]
+if test ! $(which brew)
 then
-  return
+
+  if test "$(uname -s)" = "Darwin"
+  then
+    echo "> Installing brew"
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+
+    brew bundle
+  elif test "$(uname -s)" = "Linux"
+  then
+    echo "> Installing lixuxbrew"
+
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"  > /tmp/homebrew-install.log
+  fi
 fi
-
-echo "> Installing brew"
-
-# Check if we already have Homebrew installed
-type -P brew &>/dev/null ||
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
-
-brew bundle
