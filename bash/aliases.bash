3565670c9aa4210e206c3aacccc9fee608cf9ab8
Filipe Guerra <alias.mac@gmail.com>
2017-08-15 00:29:54 -0700

    Time to commit this... 🚨

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -22,7 +22,7 @@ alias rm='rm -v'
 alias reload='. ~/.bash_profile'
 
 # make me a password
-alias genpasswd='echo `env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 10`'
+alias genpasswd='echo `env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 20`'
 
 # copy the public key
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
