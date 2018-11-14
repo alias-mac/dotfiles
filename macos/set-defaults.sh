5922ef8f35e7ccd76c132b1bc95656c3572e4d52
Filipe Guerra <alias.mac@gmail.com>
2018-11-13 17:03:02 -0800

    Fix keyboard shortcuts

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -69,9 +69,11 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Change "Move focus to next window" shortcut to ⌘§
-#/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:51:value:parameters:0 167" ~/Library/Preferences/com.apple.symbolichotkeys.plist
-# and "Move focus to the widnow drawer" shorcut to ⌥⌘§
-#/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:51:value:parameters:1 10" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+# and "Move focus to the window drawer" shorcut to ⌥⌘§
+/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:51:value:parameters:0 39" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:27:value:parameters:0 96" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+# Disable "Select the previous input source shortcut"
+/usr/libexec/PlistBuddy -c "Set :AppleSymbolicHotKeys:60:enabled 0" ~/Library/Preferences/com.apple.symbolichotkeys.plist
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
