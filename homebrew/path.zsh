9f075ddd2320149a37700c25d215a0ce3888af81
Filipe Guerra <alias.mac@gmail.com>
2022-02-11 16:45:13 -0800

    feat: add brew support for M1 macs

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,6 +1,9 @@
-#
+
+# macOS for M1 (ARM) machines needs special setup
+
+[[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
+
 # Linuxbrew needs special paths defined
-#
 
 [ "$(uname -s)" != "Linux" ] && return
 
