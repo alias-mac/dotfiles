ad93c4044705b9c0aadaf1ad0c298964f76c016f
Filipe Guerra <alias.mac@gmail.com>
2022-02-15 20:17:43 -0800

    feat: add chezmoi setup

---
+++ .chezmoiscripts/run_once_after_99-show-next-steps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Shown once after the first chezmoi apply.
+cat << 'EOF'
+
+✓ Dotfiles applied. Reload your shell:
+
+    source ~/.zshenv
+
+EOF
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -20,6 +20,7 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
+setopt HIST_IGNORE_SPACE # commands prefixed with space won't be recorded
 setopt HIST_REDUCE_BLANKS # remove superfluous blanks
 
 # don't expand aliases _before_ completion has finished
